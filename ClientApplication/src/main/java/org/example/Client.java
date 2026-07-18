2e57c255814a72ab4bca1eff81dd6c49b8f41035
claudiu <cperdevara@gmail.com>
2026-07-18 13:56:13 +0300

    Completed the Docker task

--- ClientApplication/src/main/java/org/example/Client.java
+++ ClientApplication/src/main/java/org/example/Client.java
@@ -9,62 +9,80 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
-//how to run: java -cp out org.example.Client 6767
-
-public class Client {
-    public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Command syntax: java Client <SERVER_PORT> [COMMAND_INDEX] [ARGUMENTS...]");
+public class Client
+{
+    public static void main(String[] args)
+    {
+        if(args.length < 2)
+        {
+            System.out.println("Command syntax: java Client <SERVER_HOST> <SERVER_PORT> [COMMAND_INDEX] [ARGUMENTS...]");
             return;
         }
 
+        String host = args[0];
         int port;
-        try {
-            port = Integer.parseInt(args[0]);
-        } catch (NumberFormatException e) {
-            System.out.println("Error: specify the server port!");
+        try
+        {
+            port = Integer.parseInt(args[1]);
+        }
+        catch(NumberFormatException e)
+        {
+            System.out.println("Error: specify a valid server port!");
             return;
         }
 
-        try (
-                Socket socket = new Socket("127.0.0.1", port);
+        try(
+                Socket socket = new Socket(host, port);
                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 Scanner scanner = new Scanner(System.in)
-        ) {
-            if (args.length > 1)
+        )
+        {
+            if(args.length > 2)
             {
                 readServerData(in, false);
-                String commandIndex = args[1];
+                String commandIndex = args[2];
 
-                if (commandIndex.equals("4")) {
-                    if (args.length < 3) {
-                        System.out.println("Error: Please provide the absolute path to the zip file as the 3rd argument.");
+                if(commandIndex.equals("4"))
+                {
+                    if(args.length < 4)
+                    {
+                        System.out.println("Error: Please provide the absolute path to the zip file as the 4th argument.");
                         out.println("exit");
                         return;
                     }
-                    String zipFilePath = args[2];
+                    String zipFilePath = args[3];
 
-                    try {
+                    try
+                    {
                         byte[] bytes = Files.readAllBytes(Paths.get(zipFilePath));
 
                         out.println("4 " + bytes.length);
 
                         String ack = in.readLine();
-                        if (ack != null && ack.equals("ACK")) {
+                        if(ack != null && ack.equals("ACK"))
+                        {
                             socket.getOutputStream().write(bytes);
                             socket.getOutputStream().flush();
-                        } else {
+                        }
+                        else
+                        {
                             System.out.println("Error: Server not ready to send zip!");
                         }
-                    } catch (Exception e) {
+                    }
+                    catch(Exception e)
+                    {
                         System.out.println("Error reading the ZIP file: " + e.getMessage());
                     }
-                } else {
+                }
+                else
+                {
                     StringBuilder sb = new StringBuilder();
-                    for (int i = 1; i < args.length; i++) {
+                    for(int i = 2; i < args.length; i++)
+                    {
                         sb.append(args[i]);
-                        if (i < args.length - 1) {
+                        if(i < args.length - 1)
+                        {
                             sb.append(" ");
                         }
                     }
@@ -75,52 +93,64 @@ public class Client {
 
                 out.println("exit");
             }
-            //INTERACTIVE MODE
+            // INTERACTIVE MODE
             else
             {
                 System.out.println("Successfully connected to server!");
 
                 readServerData(in, true);
 
-                while (true)
+                while(true)
                 {
                     System.out.print("> ");
                     String userInput = scanner.nextLine().trim();
                     String message = userInput;
 
-                    if (userInput.equals("3")) {
+                    if(userInput.equals("3"))
+                    {
                         System.out.println("Please write a location: ");
                         String location = scanner.nextLine();
 
                         message = message + " " + location;
                         out.println(message);
-                    } else if (userInput.equals("4")) {
+                    }
+                    else if(userInput.equals("4"))
+                    {
                         System.out.println("Please write the absolute path to a ZIP file: ");
                         String zipPath = scanner.nextLine();
 
-                        try {
+                        try
+                        {
                             byte[] bytes = Files.readAllBytes(Paths.get(zipPath));
 
                             out.println("4 " + bytes.length);
 
                             String ack = in.readLine(); //waiting for ACK signal from server
 
-                            if (ack != null && ack.equals("ACK")) {
+                            if(ack != null && ack.equals("ACK"))
+                            {
                                 socket.getOutputStream().write(bytes);
                                 socket.getOutputStream().flush();
                                 System.out.println("Uploading file ");
-                            } else {
+                            }
+                            else
+                            {
                                 System.out.println("Error: Server not ready to send zip!");
                             }
-                        } catch (Exception e) {
+                        }
+                        catch(Exception e)
+                        {
                             System.out.println("Error reading the ZIP file: " + e.getMessage());
                             continue;
                         }
-                    } else {
+                    }
+                    else
+                    {
                         out.println(message);
                     }
 
-                    if (userInput.trim().equals("exit") || userInput.trim().equals("5")) {
+                    if(userInput.trim().equals("exit") || userInput.trim().equals("5"))
+                    {
                         System.out.println("Disconnecting!");
                         break;
                     }
@@ -131,7 +161,9 @@ public class Client {
                     scanner.nextLine();
                 }
             }
-        } catch (Exception e) {
+        }
+        catch(Exception e)
+        {
             System.out.println("Error: " + e.getMessage());
         }
     }
@@ -141,10 +173,12 @@ public class Client {
         String line;
         while((line = in.readLine()) != null)
         {
-            if(line.equals("EOF") || line.equals("Server closing!")) {
+            if(line.equals("EOF") || line.equals("Server closing!"))
+            {
                 break;
             }
-            if(print) {
+            if(print)
+            {
                 System.out.println(line);
             }
         }
